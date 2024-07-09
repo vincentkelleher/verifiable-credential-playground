99f63ee670c63337eded072574cfef60d104cbe6
Vincent Kelleher <vincent.kelleher@gaia-x.eu>
2024-07-09 11:29:32 +0200

    Improve the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Container, CopyButton, Group, JsonInput, Paper, SimpleGrid, Text, Title } from '@mantine/core'
+import { Anchor, Button, Container, CopyButton, Group, JsonInput, Paper, SimpleGrid, Text, Title } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import React, { useState } from 'react'
 
@@ -80,12 +80,18 @@ export default function Home() {
       <Container>
         <SimpleGrid cols={1}>
           <Title m="lg" ta="center">
-            Gaia-X Testing Ground
+            Verifiable Credential Playground
           </Title>
           <Paper shadow="xs" p="xl">
             <Text ta="justify">
               This is only a <strong>testing/development instance</strong> used to provide a DID document for verifiable
               credential signing and verification.
+            </Text>
+            <Text mt="md" ta="justify">
+              Verifiable credentials are encoded using VC-JWT according to&nbsp;
+              <Anchor href="https://www.w3.org/TR/vc-jose-cose/" target="_blank">
+                W3C's Securing Verifiable Credentials using JOSE and COSE
+              </Anchor>.
             </Text>
           </Paper>
           <Paper shadow="xs" p="xl">
