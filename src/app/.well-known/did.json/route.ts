d211d8da6ee096f18031481ce6f5323e9d3228dc
Vincent Kelleher <vincent.kelleher@gaia-x.eu>
2024-07-09 11:50:20 +0200

    Fix DID document caching issue

--- src/app/.well-known/did.json/route.ts
+++ src/app/.well-known/did.json/route.ts
@@ -1,5 +1,6 @@
 import { createDidDocument } from '@gaia-x/did-web-generator'
 
+export const revalidate = 60
 export async function GET() {
   const didDocument = await createDidDocument(
     `https://${process.env.DOMAIN!}`,
